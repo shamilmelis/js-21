8e80e8402b6db1c2ff53fc05fa277c5766fe7bf6
shamilmelis <akastramormoley1st@mail.ru>
2023-07-17 12:24:49 +0600

    i've done my homework with array,filter,map,[...,...]

---
+++ main.js
@@ -0,0 +1,48 @@
+/* Задание 1 */
+// let arr1 = [1, 2, 3, 4, 5, 6]
+// console.log(arr1.map(el => [el]))
+
+/* Задание 2 */
+// let arr = [1, 2, 3, 4, 5, 6];
+// console.log(arr.map(el => el > 0 ? el + ' ' + '<= число больше 0' : el + ' ' + '<= число меньше 0'))
+
+/* Задание 3 */
+// let arr = [1, 2, -3, 4, -5, 6]
+// console.log(arr.map(el => el < -el ? el + ' ' + '<= это отрицательное число' : el))
+
+
+/* Задание 4 */
+// let arr = [1, 2, -3, 0, 4, 5, -6];
+// console.log(arr.filter(el => el < -el))
+
+/* Задание 5 */
+// let arr = [1, -2, 3, 0, 4, -5, 6]
+// console.log(arr.filter(el => el % 2 === 0))
+
+/* Задание 6 */
+// let arr = ['hello', 'sit', 'ipsum', 'javascript', 'python', 'sun', 'even']
+// console.log(arr.filter(el => el.length > 5))
+
+/* Задание 7 */
+// let arr = [1, 2, -3, 0, -4, 5, -6];
+// console.log(arr.filter(el => el < -el).length)
+
+/* Задание 8 */
+// let arr = [1, 2, 3, 4]
+// let str = ['a', 'b', 'c', 'd']
+// console.log([...arr,...str])
+
+/* Задание 9 */
+// let arr = [2, 3, 4, 5];
+// let str = 'hello world';
+// console.log([...arr,str])
+
+/* Задание 10 */
+// let arr = [2, 3, 4, 5];
+// let str = 'hello world';
+// console.log([...arr,...str])
+
+/* Задание 11 */
+// let arr = [2, 3, 4, 5];
+// let str = 'hello world';
+// console.log([...arr,...str,...arr])
